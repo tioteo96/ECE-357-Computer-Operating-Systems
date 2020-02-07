56ada8c27af78f66ca83761dd20bddb2bddf6c9f
Theo Song <ohmytheo@gmail.com>
2020-02-07 16:38:04 -0500

    OS

---
+++ program8/cmake-build-debug/CMakeFiles/program8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program8.dir/main.c.o"
+  "program8"
+  "program8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
